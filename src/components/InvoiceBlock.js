12197b9bfaac8cf0b444ff67cdce5456e71de2ce
Jeff <iambansah@gmail.com>
2022-02-08 10:24:33 +0000

    do dates

--- src/components/InvoiceBlock.js
+++ src/components/InvoiceBlock.js
@@ -20,6 +20,56 @@ const InvoiceBlock = () => {
                     />
                 </div>
             </div>
+
+            <div className="row mt-2">
+                <div className="col-md-6">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <Inputs
+                                type="text"
+                                className="input-placeholders-small"
+                                value="Bill To"
+                            />
+                            <Txtarea 
+                                placeholder = "Who is this invoice to? (required)"
+                                className = "textarea-visible w-100"
+                            ></Txtarea>
+                        </div>
+                        <div className="col-md-6">
+                            <Inputs
+                                    type="text"
+                                    className="input-placeholders-small "
+                                    value="Ship To"
+                                />
+                                <Txtarea 
+                                    placeholder = "Optional"
+                                    className = "textarea-visible w-100"
+                                ></Txtarea>                            
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-6 p-0">
+                    <div className="row p-0 justify-content-end d-flex">
+                        <div className="col-md-7 justify-content-end d-flex">
+                                    <Inputs
+                                        type="text"
+                                        className="input-placeholders-small text-end border"
+                                        value="Ship To"
+                                    />                            
+                        </div>
+                        <div className="col-md-5 justify-content-end d-flex"> 
+                                 <Inputs
+                                    type="text"
+                                    className="input-placeholders-small"
+                                    value="Ship To"
+                                />                            
+                        </div>
+                    </div>
+
+                    
+                </div>
+            </div>            
         </div>
     )
 }
