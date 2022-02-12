89e1282ae985361d9d1d9fa702d3e6c1017375c5
Jeff <iambansah@gmail.com>
2022-02-12 21:27:13 +0000

    dynamic table columns

--- src/components/InvoiceTable.js
+++ src/components/InvoiceTable.js
@@ -2,20 +2,43 @@ import React from 'react';
 
 function InvoiceTable () {
     return (
-            <div className="desc">
-                <div className="item">
+        <>
+            <div className="header row desc mt-5">
+                <div className="col-md-5">
                     <input type="text" name="" id="" />
                 </div>
-                <div className="quantity">
+                <div className="col-md-2 ">
                     <input type="text" name="" id="" />
                 </div>
-                <div className="rate">
+                <div className="col-md-2 ">
                     <input type="text" name="" id="" />
                 </div>
-                <div className="amount">
+                <div className="col-md-2 ">
                     <input type="text" name="" id="" />
+                </div>
+                <div className="col-md-1">
+                    {/* <input type="text" name="" id="" /> */}
                 </div>
             </div>
+            <div className="dynamicTable row ">
+                <div className="col-md-5">
+                    <input type="text" name="" id="" />
+                </div>
+                <div className="col-md-2">
+                    <input type="text" name="" id="" />
+                </div>
+                <div className="col-md-2">
+                    <input type="text" name="" id="" />
+                </div>
+                <div className="col-md-2 ">
+                    <input type="text" name="" id="" />
+                </div>
+                <div className="col-md-1 ">
+                    <input type="text" name="" id="" />
+                </div>                
+            </div>
+            
+        </>                
     )
 }
 
