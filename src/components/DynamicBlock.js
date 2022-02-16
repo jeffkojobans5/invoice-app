02ca5de1a499cfc7e4ce53f11321e25e8ecc5648
Jeff <iambansah@gmail.com>
2022-02-16 16:54:45 +0000

    setup context

--- src/components/DynamicBlock.js
+++ src/components/DynamicBlock.js
@@ -1,26 +1,61 @@
-import { FaTrashAlt } from 'react-icons/fa';
+import React from 'react'
+import {  FaTrashAlt } from 'react-icons/fa';
+import Inputs from '../props/Inputs'
+import { v4 as uuidv4 } from 'uuid';
 
 function DynamicBlock () {
 
+    const [inputFields , setInputField] = React.useState([
+        { 
+            fieldDetails : [ { id: uuidv4(), description : '' , quantity : 2 , rate : 2 , total : 4 }  ]
+        }
+    ])
+
+      
     return (
         <>
-            <div className="dynamicTable row ">
+        { inputFields.map((inputField , index)=> { 
+
+            return (
+            <div className="dynamicTable row " key={inputField.fieldDetails[index]['id']}>
                 <div className="col-md-5">
-                    <input type="text" name="" id="" />
+                    <Inputs
+                        name="firstName"
+                        label="First Name"
+                        value={inputField.fieldDetails[index]['description']}
+                        // onChange={event => (inputField.id, event)}
+                    />
                 </div>
                 <div className="col-md-2">
-                    <input type="text" name="" id="" />
+                    <Inputs
+                            name="firstName"
+                            label="First Name"
+                            value={inputField.fieldDetails[index]['quantity']}
+                            // onChange={event => (inputField.id, event)}
+                    />                
                 </div>
                 <div className="col-md-2">
-                    <input type="text" name="" id="" />
+                    <Inputs
+                                name="firstName"
+                                label="First Name"
+                                value={inputField.fieldDetails[index]['rate']}
+                                // onChange={event => (inputField.id, event)}
+                        />   
                 </div>
                 <div className="col-md-2 ">
-                    <input type="text" name="" id="" />
+                    <Inputs
+                                name="firstName"
+                                label="First Name"
+                                value={inputField.fieldDetails[index]['amount']}
+                                // onChange={event => (inputField.id, event)}
+                        />   
                 </div>
                 <div className="col-md-1 d-flex justify-content-center align-items-center">
                     <FaTrashAlt />
                 </div>                
-            </div>            
+            </div>      
+            
+            )}) }
         </>
     )
 }
