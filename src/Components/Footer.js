c0f2598ba3b04258fd515a7f8ce65731649054ba
Jeff Bans <iambansah@gmail.com>
2022-07-13 19:37:56 +0000

    link to site : footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -12,7 +12,7 @@ function Footer () {
                             <p className="text-center developers"> <a href="github.com/jeffkojobans5/invoice-app"> Developers </a> </p>
                         </div>                     
                         <div className="col-md-4 p-3">
-                            <p className="text-end"> Made by <a href="https://jeffkojobans.vercel.app"> @jeffkojobans </a> Accra - Ghana. </p>
+                            <p className="text-end"> Made by <a href="https://jeff-kojo-bans.vercel.app"> @jeffkojobans </a> Accra - Ghana. </p>
                         </div>                     
                     </div>
                 </div>
